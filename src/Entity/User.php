1ff727644a7866d75eb1b7b3c694395667753e78
RecipesFYP <recipes.fyp@gmail.com>
2018-05-11 16:28:44 +0100

    Added the FOSUserBundle for User Registration etc Modified some of the templates configured some of the scurity settings Added a Docs folder for Project Documentation

---
+++ src/Entity/User.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="`user`")
+ */
+class User extends BaseUser {
+	
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+     protected $id;
+
+}
